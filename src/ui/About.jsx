5eb4ae643834b9c92447fa1f48743765e8599f2a
sahanherath25 <sahanherath444@gmail.com>
2025-07-29 18:14:59 +0530

    Testing Minor Change of Content -Sahan

--- src/ui/About.jsx
+++ src/ui/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
 
     return (
         <div >
-            <p>This is the About section of the website.</p>
+            <p>This is the About section of the website</p>
         </div>
     );
 };
